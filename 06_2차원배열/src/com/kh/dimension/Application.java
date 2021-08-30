39193a92b6ebaf082a88699b3e45c2fd8215e903
soheepark <47832401+soheepark05@users.noreply.github.com>
2021-08-31 01:47:09 +0900

    06_2차원배열
    실습은 중복o까지만함,,
    내일 중복제외코드도 수정하기!

---
+++ 06_2차원배열/src/com/kh/dimension/Application.java
@@ -0,0 +1,15 @@
+package com.kh.dimension;
+
+import com.kh.dimension.practice.DimensionalArray;
+
+public class Application { //어플리케이션 부분
+
+	public static void main(String[] args) {
+
+		//new DimensionalArray().method1(); //메소드1
+		//new DimensionalArray().method2(); //메소드2
+		new DimensionalArray().method3(); //메소드3
+		
+	}
+
+}
